8c7a299cd2a1253dba1f421a72227f5ebd17b464
Sahachai Y <three_d1995@hotmail.co.th>
2023-02-17 17:35:02 +0700

    build: 📦 first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# to-color-string
+
+Convert text to hex color codes with JS `String.prototype` method.
+
+## Installation
+
+### In a browser
+
+```
+<script src="to-color-string/dist/tocolorstring.js"></script>
+```
+
+### Using npm
+
+```
+npm i to-color-string
+```
+
+## Usage
+
+### String.toColorString()
+
+```
+import "to-color-string";
+
+// case: empty string
+console.log("".toColorString()); 
+// "#d41d8c"
+
+// case: single character
+console.log("h".toColorString()); 
+// "2510c3"
+
+// case: sentence
+console.log(`Hello, is it me you're looking for?`.toColorString()); 
+// "b1f23d"
+```
+
+## Test
+
+```
+npm i -D
+npm run test
+```
+
+## License
+
+This project is licensed under the terms of the MIT license.
---
+++ test/test.js
@@ -0,0 +1,39 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+require("../src/index");
+
+describe("to-color-string", function () {
+    describe("Function `String.prototype.toColorString()`", function () {
+
+        it("case: empty string", function () {
+            let testString = '';
+            let expectString = '#d41d8c';
+            expect(testString.toColorString()).to.equal(expectString);
+        });
+
+        it("case: single character", function () {
+            let testString = 'h';
+            let expectString = '#2510c3';
+            expect(testString.toColorString()).to.equal(expectString);
+        });
+
+        it("case: word", function () {
+            let testString = 'hello';
+            let expectString = '#5d4140';
+            expect(testString.toColorString()).to.equal(expectString);
+        });
+
+        it("case: sentence", function () {
+            let testString = `Hello, is it me you're looking for?`;
+            let expectString = '#b1f23d';
+            expect(testString.toColorString()).to.equal(expectString);
+        });
+
+        it("case: non english sentence", function () {
+            let testString = `สวัสดีครับ ท่านสมาชิกชมรมคนชอบหมี`;
+            let expectString = '#af85ed';
+            expect(testString.toColorString()).to.equal(expectString);
+        });
+    });
+});
